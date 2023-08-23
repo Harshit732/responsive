bf851f0eaad75c06fddfaa6e64b8f7ef470ce452
Harshit Srivastava <harshit00732@gmail.com>
2023-08-23 14:02:50 +0530

    git created

---
+++ index.js
@@ -0,0 +1,10 @@
+const arrowIcons =document.querySelectorAll("i").addEventListener("click",function(){
+    var k=this.innerHTML
+    slider(k);});
+    
+function slider(key)
+{
+    console.log("console");
+    alert("more slides needed");
+}
+
